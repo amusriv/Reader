bb1c3752fa7c73ac1fc394e8b2b6864e8daa570a
unknown <amulyas@MININT-N107LF2.ant.amazon.com>
2015-07-28 12:05:40 +0530

    standAloneWebpage... contains the HTML+CSS; TODO:obfiscate the javascript

--- src/com/example/myproject/ReaderServlet.java
+++ src/com/example/myproject/ReaderServlet.java
@@ -1,46 +1,36 @@
 package com.example.myproject;
 import java.io.IOException;
-
-import com.reader.Reader.*;
+import com.google.gson.Gson;
 
 import javax.servlet.*;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.http.*;
-
 import com.reader.Reader.dbAccess;
+import com.reader.Reader.GotoLink;
 
 //@WebServlet("/ReaderServlet")
 @SuppressWarnings("serial")
-
 public class ReaderServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		/*resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");*/
-		
-		RequestDispatcher view = req.getRequestDispatcher("index.html");
-		view.forward(req, resp);
-		
-	}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        //MyClass myClass = new MyClass();
 		System.out.println("\n\n\n!!!!!POST REACHED!!!!\n\n\n");		
 		dbAccess db = new dbAccess();	
 		
+		GotoLink link = new GotoLink();
+		link.setUrl(db.getLink());
+		Gson gson = new Gson();
+		resp.setContentType("application/json");		
+		gson.toJson(link);
+		//resp.getWriter().print(gson.toJson(link));	    
+		System.out.println("\n\n\n" +  gson.toJson(link) + "\n\n\n\n");
+		resp.getWriter().print(gson.toJson(link));
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        
+		System.out.println("\n\n\n!!!!!POST REACHED!!!!\n\n\n");		
+		dbAccess db = new dbAccess();			
 		response.sendRedirect(db.getLink());
-	      
-        /*if (request.getParameter("button1") != null) {
-            myClass.function1();
-        } else if (request.getParameter("button2") != null) {
-            myClass.function2();
-        } else if (request.getParameter("button3") != null) {
-            myClass.function3();
-        } else {
-            // ???
-        }*/
-
-        //request.getRequestDispatcher("/WEB-INF/some-result.jsp").forward(request, response);
-    }
+		System.out.println("REDIRECT SENT!");
+	}
 
 	
 }
--- src/com/reader/Reader/dbAccess.java
+++ src/com/reader/Reader/dbAccess.java
@@ -1,80 +1,70 @@
 package com.reader.Reader;
 
-//STEP 1. Import required packages
 import java.sql.*;
 import java.util.Random;
 
-//import javax.sql.DataSource;
-//import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
-
-
 public class dbAccess {
- // JDBC driver name and database URL
- static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
- static final String DB_URL = "jdbc:mysql://localhost/story_db";
+	// JDBC driver name and database URL
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost/story_db";
 
- //  Database credentials
- static final String USER = "root";
- static final String PASS = "hotpie321";
- String url = "www.google.com";
- public static void main(String[] args) {}
- public String getLink(){
- Connection conn = null;
- Statement stmt = null;
- try{
-    //STEP 2: Register JDBC driver
-    Class.forName("com.mysql.jdbc.Driver");
+	// Database credentials
+	static final String USER = "root";
+	static final String PASS = "hotpie321";
+	String url = "www.google.com";
 
-    //STEP 3: Open a connection
-    System.out.println("Connecting to database...");
-    conn = DriverManager.getConnection(DB_URL, USER, PASS);
-    
-    Random randomGenerator = new Random();
-    int num = randomGenerator.nextInt(55)+1;
-    stmt=conn.createStatement();
-    String sql = "SELECT url FROM stories WHERE sno =" + num;
-    
-    System.out.println("\n\n\n FETCHING RESULT SET \n\n\n");
-    
-    ResultSet rs = stmt.executeQuery(sql);
-    rs.next();
-    
-    
-    
-    url = rs.getString("url");
-    System.out.println("\n\n\n RESULT SET IS : \n\n\n" + url);
-    
-    
-   /* //STEP 4: Execute a query
-    System.out.println("Creating database...");
-    stmt = conn.createStatement();
-    
-    String sql = "CREATE DATABASE STUDENTS";
-    stmt.executeUpdate(sql);
-    System.out.println("Database created successfully...");*/
- }catch(SQLException se){
-    //Handle errors for JDBC
-    se.printStackTrace();
-    url="www.google.com";
- }catch(Exception e){
-    //Handle errors for Class.forName
-    e.printStackTrace();
-    url="www.google.com";
- }finally{
-    //finally block used to close resources
-    try{
-       if(stmt!=null)
-          stmt.close();
-    }catch(SQLException se2){ url="www.google.com";
-    }// nothing we can do
-    try{
-       if(conn!=null)
-          conn.close();
-    }catch(SQLException se){ url="www.google.com";
-       se.printStackTrace();
-    }//end finally try
- }//end try
- return url;
- //System.out.println("Goodbye!");
-}//end main
-}//end JDBCExample
+	public static void main(String[] args) {
+	}
+
+	public String getLink() {
+		Connection conn = null;
+		Statement stmt = null;
+		try {
+			// STEP 2: Register JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// STEP 3: Open a connection
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+			Random randomGenerator = new Random();
+			int num = randomGenerator.nextInt(55) + 1;
+			stmt = conn.createStatement();
+			String sql = "SELECT url FROM stories WHERE sno =" + num;
+
+			System.out.println("\n\n\n FETCHING RESULT SET \n\n\n");
+
+			ResultSet rs = stmt.executeQuery(sql);
+			rs.next();
+
+			url = rs.getString("url");
+			System.out.println("\n\n\n RESULT SET IS : \n\n\n" + url);
+
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+			url = "www.google.com";
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+			url = "www.google.com";
+		} finally {
+			// finally block used to close resources
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException se2) {
+				url = "www.google.com";
+			}// nothing we can do
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException se) {
+				url = "www.google.com";
+				se.printStackTrace();
+			}
+		}
+		return url;
+		
+	}
+}
