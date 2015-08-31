40b8f051bb8ee1ec4bb95c54fff6f8299a97b658
Pushkar <pushkar1593@gmail.com>
2015-08-31 23:26:14 +0530

    Generated the angular app in ./reader

---
+++ reader/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name readerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the readerApp
+ */
+angular.module('readerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ reader/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name readerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the readerApp
+ */
+angular.module('readerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
